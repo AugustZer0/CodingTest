e91951cfcedb3af9cdc07fa894631650920bc8c1
AugustZer0 <qweraz157890@gmail.com>
2026-06-18 18:49:39 +0900

    Section 2 복습 및 Doc 추가

--- Section2/6.py
+++ Section2/6.py
@@ -1,28 +1,47 @@
+"""
+자릿수의 합
+
+N개의 자연수가 입력되면 각 자연수의 자릿수의 합을 구하고, 그 합이 최대인 자연수를 출력
+하는 프로그램을 작성하세요. 각 자연수의 자릿수의 합을 구하는 함수를 def digit_sum(x)를
+꼭 작성해서 프로그래밍 하세요.
+
+▣ 입력설명
+첫 줄에 자연수의 개수 N(3<=N<=100)이 주어지고, 그 다음 줄에 N개의 자연수가 주어진다.
+각 자연수의 크기는 10,000,000를 넘지 않는다.
+
+▣ 출력설명
+자릿수의 합이 최대인 자연수를 출력한다. 자릿수의 합이 같을 경우 입력순으로 먼저인 숫자
+를 출력합니다.
+
+▣ 입력예제 1
+3
+125 15232 97
+
+▣ 출력예제 1
+97
+
+
+"""
+
 def digit_sum(x):
-    res = 0
+    result = 0
     for digit in x:
-        res += int(digit)
-    return res
+        result += int(digit)
+    return result
+
+
 
 n = int(input())
-a = list(input().split())
+
+arr = list(input().split())
 
 max_sum = -1
-ans = ""
 
-for num in a:
-    current_sum = digit_sum(num)
-    
+for i in arr:
+    current_sum = digit_sum(i)
+
     if current_sum > max_sum:
         max_sum = current_sum
-        ans = num
-
-print(ans)
-
-a = ["125", "15232", "97"]
-
-for x in a:
-    res = 0
-    for digit in x:
-        res += int(digit)
-    print(res)
+        answer = i
+    
+print(answer)
